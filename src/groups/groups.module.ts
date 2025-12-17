77162177a6845f62abf84140a5f49c8b3a06ad5a
tehila.ch <tehila.ch12@gmail.com>
2025-12-17 18:31:50 +0200

    fix: remove backend code from frontend

--- src/groups/groups.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GroupsService } from './groups.service';
-import { GroupsController } from './groups.controller';
-
-@Module({
-  providers: [GroupsService],
-  controllers: [GroupsController]
-})
-export class GroupsModule {}
